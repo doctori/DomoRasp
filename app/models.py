e7ad1bb2efd6f7733872ace6553ced1bbc012feb
doctori <lerouge.pierre@gmail.com>
2015-07-25 17:12:53 +0000

    working smooth actions (needs arduino project - most handling it's done on it's way)

--- app/models.py
+++ app/models.py
@@ -1,34 +1,93 @@
 from app import db
 from sqlalchemy.ext.declarative import declarative_base as real_declarative_base
 from datetime import datetime
+import time
+import sys
+from RF24 import *
 
-class Controler(db.Model):
-	id = db.Column(db.Integer,primary_key=True)
-	name = db.Column(db.String(64), index=True,unique=True)
-	status = db.Column(db.Integer)
-	description = db.Column(db.Text)
-	elements = db.relationship('Element', backref='controler', lazy='dynamic')
-	addedAt = db.Column(db.DateTime)
-	def __repr__(self):
-		return '<Controler %r>' %(self.name)
-	def __init__(self,name,status,description):
-		self.name = name
-		self.status = status
-		self.description = description
-		self.addedAt = datetime.now()
+radio = RF24(22,0)
+pipes = [ "1Node","2Node"]
+millis = lambda: int(round(time.time() * 1000))
+payloadSize = 16
+
+class Controller(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    status = db.Column(db.Integer)
+    description = db.Column(db.Text)
+    elements = db.relationship('Element', backref='controller', lazy='dynamic')
+    addedAt = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return '<Controller %r>' % (self.name)
+
+    def __init__(self, name, status, description):
+        self.name = name
+        self.status = status
+        self.description = description
+        self.addedAt = datetime.now()
+
+    def getPayload(self):
+        return "%d|%d" % (self.id, self.status)
+
+    def switch(self):
+        if self.status != 0:
+            self.status = 0
+        else:
+            self.status = 255
+        self.updateController()
+
+    def updateController(self):
+        payload = self.getPayload()
+        print('Trying To Communicate with the Arduino Nano')
+        radio.begin()
+        radio.setAutoAck(1);
+        radio.enableAckPayload();
+        radio.setRetries( 15, 15);
+
+        radio.printDetails()
+
+        radio.openWritingPipe(pipes[0])
+        radio.openReadingPipe(1,pipes[1])
+
+        while 1:
+            radio.stopListening()
+            print('Now sending ', str(payload), ' ... ',)
+            radio.write(payload)
+            radio.startListening()
+
+            # Wait here until we get a response, or timeout
+            started_waiting_at = millis()
+            timeout = False
+            while (not radio.available()) and (not timeout):
+                if (millis() - started_waiting_at) > 1000:
+                    timeout = True
+
+            # Describe the results
+            if timeout:
+                print('failed, response timed out.')
+            else:
+                # Grab the response, compare, and send to debugging spew
+                len = radio.getDynamicPayloadSize()
+                receive_payload = radio.read(len)
+
+                # Spew it
+                print('got response size=', len, ' value="', receive_payload, '"')
+                break
+            time.sleep(1)
+
 
 class Element(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    description = db.Column(db.Text())
+    status = db.Column(db.Integer)
+    lastUpdate = db.Column(db.DateTime)
+    addedAt = db.Column(db.DateTime)
+    controller_id = db.Column(db.Integer, db.ForeignKey('controller.id'))
 
-	id = db.Column(db.Integer, primary_key = True)
-	name = db.Column(db.String(64),index=True,unique=True)
-	description = db.Column(db.Text())
-	status = db.Column(db.Integer)
-	lastUpdate = db.Column(db.DateTime)
-	addedAt = db.Column(db.DateTime)
-	controler_id = db.Column(db.Integer, db.ForeignKey('controler.id'))
-	
-	def __init__(self,name,status,description):
-		self.name = name
-		self.status = status
-		self.description = description
-		self.addedAt = datetime.now()
+    def __init__(self, name, status, description):
+        self.name = name
+        self.status = status
+        self.description = description
+        self.addedAt = datetime.now()
--- app/views.py
+++ app/views.py
@@ -1,62 +1,76 @@
 from flask import redirect, url_for, jsonify, request,make_response
 from flask.ext.restful import Api, Resource, reqparse, fields, marshal
 from app import app,db
-from .models import Controler,Element
+from .models import Controller,Element
+from RF24Connector import updateController
 api = Api(app)
 controller_fields = {
-	'id': fields.Integer,
-	'name': fields.String,
-	'description': fields.String,
-	'status': fields.Integer,
+    'id': fields.Integer,
+    'name': fields.String,
+    'description': fields.String,
+    'status': fields.Integer,
 #	'uri': fields.Url('controller')
 }
-class ControlerAPI(Resource):
-	def __init__(self):
-	#Will be used to parse our answers
-		self.reqparse = reqparse.RequestParser()
-		self.reqparse.add_argument('name', type=str, location='json')
-		self.reqparse.add_argument('description', type=str, location='json')
-		self.reqparse.add_argument('status', type=int, location='json')
-		super(ControlerAPI, self).__init__()
-	def get(self,controller_id):
-		controller = Controler.query.get(controller_id)
-		return [marshal(controller, controller_fields)]
+class ControllerAPI(Resource):
+    def __init__(self):
+    #Will be used to parse our answers
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('name', type=str, location='json')
+        self.reqparse.add_argument('description', type=str, location='json')
+        self.reqparse.add_argument('status', type=int, location='json')
+        super(ControllerAPI, self).__init__()
+    def get(self,controller_id):
+        controller = Controller.query.get(controller_id)
+        return [marshal(controller, controller_fields)]
 
-	def put (self,controller_id):
-		args = self.reqparse.parse_args()
-		controller = Controler.query.get(controller_id)
-		controller.name = args['name']
-		controller.description = args['description']
-		controller.status = args['status']
-		#db.session.update(controller)
-		db.session.commit()
-	def delete(self,controller_id):
-		controller = Controler.query.get(controller_id)
-		db.session.delete(controller)
-		db.session.commit()
-	
-class ControlerListAPI(Resource):
-	def __init__(self):
-		#Will be used to parse our answers
-		self.reqparse = reqparse.RequestParser()
-		self.reqparse.add_argument('name', type=str, location='json',required=True)
-		self.reqparse.add_argument('description', type=str, location='json')
-		self.reqparse.add_argument('status', type=int, location='json')
-		super(ControlerListAPI, self).__init__()
-	def get(self):
-		controllers = Controler.query.all()
-		return [marshal(controller,controller_fields) for controller in controllers]
-	def post(self):
-		args = self.reqparse.parse_args()
-		controller = Controler(
-			name= args['name'],
-			description= args['description'],
-			status= args['status']
-		)
-		db.session.add(controller)
-		db.session.commit()
-		return {'controller': marshal(controller, controller_fields)},201
-		
-		
-api.add_resource(ControlerListAPI,'/api/controllers')
-api.add_resource(ControlerAPI,'/api/controllers/<int:controller_id>',endpoint = 'controller')
+    def put (self,controller_id):
+        args = self.reqparse.parse_args()
+        controller = Controller.query.get(controller_id)
+        controller.name = args['name']
+        controller.description = args['description']
+        controller.status = args['status']
+        print("Going to set the value of the controller to : %r" %(controller.status))
+        controller.updateController()
+        #db.session.update(controller)
+        db.session.commit()
+
+    def delete(self,controller_id):
+        controller = Controller.query.get(controller_id)
+        db.session.delete(controller)
+        db.session.commit()
+class ControllerActionsAPI(Resource):
+    def __init__(self):
+     #Will be used to parse our answers
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('name', type=str, location='json')
+        self.reqparse.add_argument('description', type=str, location='json')
+        self.reqparse.add_argument('status', type=int, location='json')
+    def get(self,controller_id,action):
+        controller = Controller.query.get(controller_id)
+
+class ControllerListAPI(Resource):
+    def __init__(self):
+        #Will be used to parse our answers
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('name', type=str, location='json',required=True)
+        self.reqparse.add_argument('description', type=str, location='json')
+        self.reqparse.add_argument('status', type=int, location='json')
+        super(ControllerListAPI, self).__init__()
+    def get(self):
+        controllers = Controller.query.all()
+        return [marshal(controller,controller_fields) for controller in controllers]
+    def post(self):
+        args = self.reqparse.parse_args()
+        controller = Controller(
+            name= args['name'],
+            description= args['description'],
+            status= args['status']
+        )
+        db.session.add(controller)
+        db.session.commit()
+        return {'controller': marshal(controller, controller_fields)},201
+
+
+api.add_resource(ControllerListAPI,'/api/controllers')
+api.add_resource(ControllerAPI,'/api/controllers/<int:controller_id>',endpoint = 'controller')
+api.add_resource(ControllerActionsAPI,'/api/controllers/<int:controller_id>/<string:action>',endpoint = 'controllerActions')
