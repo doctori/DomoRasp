ccc0eec5bbd9a8e799550813e61ad3ae1822a621
doctori <lerouge.pierre@gmail.com>
2015-07-28 22:00:28 +0000

    working element handling

--- app/models.py
+++ app/models.py
@@ -1,5 +1,4 @@
 from app import db
-from sqlalchemy.ext.declarative import declarative_base as real_declarative_base
 from datetime import datetime
 import time
 import sys
@@ -17,18 +16,19 @@ class Controller(db.Model):
     description = db.Column(db.Text)
     elements = db.relationship('Element', backref='controller', lazy='dynamic')
     addedAt = db.Column(db.DateTime)
+    pipeRead = db.Column(db.String(32))
+    pipeWrite = db.Column(db.String(32))
 
     def __repr__(self):
         return '<Controller %r>' % (self.name)
 
-    def __init__(self, name, status, description):
+    def __init__(self, name, status, description,pipeRead,pipeWrite):
         self.name = name
         self.status = status
         self.description = description
         self.addedAt = datetime.now()
-
-    def getPayload(self):
-        return "%d|%d" % (self.id, self.status)
+        self.pipeRead = pipeRead
+        self.pipeWrite = pipeWrite
 
     def switch(self):
         if self.status != 0:
@@ -39,17 +39,52 @@ class Controller(db.Model):
         db.session.commit()
 
     def updateController(self):
+        for element in self.elements:
+            element.status = self.status
+            element.updateElement()
+
+
+class Element(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    description = db.Column(db.Text())
+    status = db.Column(db.Integer)
+    lastUpdate = db.Column(db.DateTime)
+    addedAt = db.Column(db.DateTime)
+    controller_id = db.Column(db.Integer, db.ForeignKey('controller.id'))
+
+    def __init__(self, name, status, description,controller_id):
+        self.name = name
+        self.status = status
+        self.description = description
+        self.controller_id = controller_id
+        self.addedAt = datetime.now()
+
+    def getPayload(self):
+        return "%d|%d" % (self.id, self.status)
+
+    def switch(self):
+        if self.status != 0:
+            self.status = 0
+        else:
+            self.status = 255
+        self.updateElement()
+        db.session.commit()
+
+    def updateElement(self):
         payload = self.getPayload()
         print('Trying To Communicate with the Arduino Nano')
+
         radio.begin()
         radio.setAutoAck(1);
         radio.enableAckPayload();
         radio.setRetries( 15, 15);
 
         radio.printDetails()
-
-        radio.openWritingPipe(pipes[0])
-        radio.openReadingPipe(1,pipes[1])
+        print("writing to pipe : "+self.controller.pipeWrite.encode('utf-8'))
+        print("Reading to Pipe : "+self.controller.pipeRead.encode('utf-8'))
+        radio.openWritingPipe(self.controller.pipeWrite.encode('utf-8'))
+        radio.openReadingPipe(1,self.controller.pipeRead.encode('utf-8'))
 
         while 1:
             radio.stopListening()
@@ -76,20 +111,3 @@ class Controller(db.Model):
                 print('got response size=', len, ' value="', receive_payload, '"')
                 break
             time.sleep(1)
-
-
-class Element(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
-    description = db.Column(db.Text())
-    status = db.Column(db.Integer)
-    lastUpdate = db.Column(db.DateTime)
-    addedAt = db.Column(db.DateTime)
-    controller_id = db.Column(db.Integer, db.ForeignKey('controller.id'))
-
-    def __init__(self, name, status, description,controller_id):
-        self.name = name
-        self.status = status
-        self.description = description
-        self.controller_id = controller_id
-        self.addedAt = datetime.now()
--- app/views.py
+++ app/views.py
@@ -17,37 +17,12 @@ controller_fields = {
     'name': fields.String,
     'description': fields.String,
     'status': fields.Integer,
-    'elements': fields.List(fields.Nested(element_fields))
-#	'uri': fields.Url('controller')
+    'elements': fields.List(fields.Nested(element_fields)),
+    'pipeRead': fields.String,
+    'pipeWrite': fields.String
 }
 
-class ControllerAPI(Resource):
-    def __init__(self):
-    #Will be used to parse our answers
-        self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('name', type=str, location='json')
-        self.reqparse.add_argument('description', type=str, location='json')
-        self.reqparse.add_argument('status', type=int, location='json')
-        super(ControllerAPI, self).__init__()
-    def get(self,controller_id):
-        controller = Controller.query.get(controller_id)
-        return [marshal(controller, controller_fields)]
 
-    def put (self,controller_id):
-        args = self.reqparse.parse_args()
-        controller = Controller.query.get(controller_id)
-        controller.name = args['name']
-        controller.description = args['description']
-        controller.status = args['status']
-        print("Going to set the value of the controller to : %r" %(controller.status))
-        controller.updateController()
-        #db.session.update(controller)
-        db.session.commit()
-
-    def delete(self,controller_id):
-        controller = Controller.query.get(controller_id)
-        db.session.delete(controller)
-        db.session.commit()
 class ElementAPI(Resource):
     def __init__(self):
     #Will be used to parse our answers
@@ -56,10 +31,30 @@ class ElementAPI(Resource):
         self.reqparse.add_argument('description', type=str, location='json')
         self.reqparse.add_argument('status', type=int, location='json')
         self.reqparse.add_argument('controller_id', type=int, location='json')
-        super(ControllerAPI, self).__init__()
-    def put (self,controller_id,element_id):
+        super(ElementAPI, self).__init__()
+    def put (self,element_id):
         args = self.reqparse.parse_args()
         element = Element.query.get(element_id)
+        element.description = args['description']
+        element.status = args['status']
+        element.updateElement()
+    def get(self,element_id):
+        element = Element.query.get(element_id)
+        return [marshal(element, element_fields)]
+
+
+class ElementActionsAPI(Resource):
+    def __init__(self):
+     #Will be used to parse our answers
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('name', type=str, location='json')
+        self.reqparse.add_argument('description', type=str, location='json')
+        self.reqparse.add_argument('status', type=int, location='json')
+    def get(self,element_id,action):
+        element = Element.query.get(element_id)
+        if action == 'switch':
+            element.switch()
+
 class ElementListAPI(Resource):
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
@@ -81,6 +76,7 @@ class ElementListAPI(Resource):
         db.session.add(element)
         db.session.commit()
         return {'controller': marshal(element, element_fields)},201
+
 class ControllerActionsAPI(Resource):
     def __init__(self):
      #Will be used to parse our answers
@@ -88,6 +84,8 @@ class ControllerActionsAPI(Resource):
         self.reqparse.add_argument('name', type=str, location='json')
         self.reqparse.add_argument('description', type=str, location='json')
         self.reqparse.add_argument('status', type=int, location='json')
+        self.reqparse.add_argument('pipeRead', type=str, location='json')
+        self.reqparse.add_argument('pipeWrite', type=str, location='json')
     def get(self,controller_id,action):
         controller = Controller.query.get(controller_id)
         if action == 'switch':
@@ -101,6 +99,8 @@ class ControllerListAPI(Resource):
         self.reqparse.add_argument('name', type=str, location='json',required=True)
         self.reqparse.add_argument('description', type=str, location='json')
         self.reqparse.add_argument('status', type=int, location='json')
+        self.reqparse.add_argument('pipeRead', type=str, location='json')
+        self.reqparse.add_argument('pipeWrite', type=str, location='json')
         super(ControllerListAPI, self).__init__()
     def get(self):
         controllers = Controller.query.all()
@@ -110,15 +110,53 @@ class ControllerListAPI(Resource):
         controller = Controller(
             name= args['name'],
             description= args['description'],
-            status= args['status']
+            status= args['status'],
+            pipeRead = args['pipeRead'],
+            pipeWrite = args['pipeWrite']
         )
         db.session.add(controller)
         db.session.commit()
         return {'controller': marshal(controller, controller_fields)},201
 
+class ControllerAPI(Resource):
+    def __init__(self):
+    #Will be used to parse our answers
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('name', type=str, location='json')
+        self.reqparse.add_argument('description', type=str, location='json')
+        self.reqparse.add_argument('status', type=int, location='json')
+        self.reqparse.add_argument('pipeRead', type=str, location='json')
+        self.reqparse.add_argument('pipeWrite', type=str, location='json')
+        super(ControllerAPI, self).__init__()
+
+    def get(self,controller_id):
+        controller = Controller.query.get(controller_id)
+        return [marshal(controller, controller_fields)]
+
+    def put(self,controller_id):
+        args = self.reqparse.parse_args()
+        controller = Controller.query.get(controller_id)
+        controller.name = args['name']
+        controller.description = args['description']
+        controller.status = args['status']
+        controller.pipeRead = args['pipeRead']
+        controller.pipeWrite = args['pipeWrite']
+        print("Going to set the value of the controller to : %r" %(controller.status))
+        for element in controller.elements:
+            print(element)
+        controller.updateController()
+        #db.session.update(controller)
+        db.session.commit()
+
+    @staticmethod
+    def delete(self,controller_id):
+        controller = Controller.query.get(controller_id)
+        db.session.delete(controller)
+        db.session.commit()
 
 api.add_resource(ControllerListAPI,'/api/controllers')
 api.add_resource(ControllerAPI,'/api/controllers/<int:controller_id>',endpoint = 'controller')
 api.add_resource(ControllerActionsAPI,'/api/controllers/<int:controller_id>/<string:action>',endpoint = 'controllerActions')
 api.add_resource(ElementAPI,'/api/elements/<int:element_id>',endpoint = 'element')
 api.add_resource(ElementListAPI,'/api/elements')
+api.add_resource(ElementActionsAPI,'/api/elements/<int:element_id>/<string:action>',endpoint = 'elementActions')
